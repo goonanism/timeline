1ed9bb1477ae1a54772607b6c59b8ccca8dd47fb
Hammy Goonan <hammy@spiresoftware.com.au>
2014-04-17 21:40:58 +0300

    first commit - following tutorial but bailing out of it

---
+++ schema.sql
@@ -0,0 +1,22 @@
+drop table if exists event;
+create table event (
+  id integer primary key autoincrement,
+  name text not null,
+  note text null,
+  link text null,
+  date date null,
+  milestone integer null
+);
+
+drop table if exists tag;
+create table tag (
+  id integer primary key autoincrement,
+  name text not null
+);
+
+drop table if exists event_tag;
+create table event_tag (
+  id integer primary key autoincrement,
+  event_id integer not null,
+  tag_id integer not null
+);
