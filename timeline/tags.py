d84723dd732ddb8092e6c6396b1872d7622d8cf5
Hammy Goonan <hammy@spiresoftware.com.au>
2014-05-15 22:25:46 +0100

    added recursive option to getting events

--- timeline/tags.py
+++ timeline/tags.py
@@ -8,21 +8,25 @@ from database import Database
 class Tag():
 	def __init__(self):
 		self.db = Database()
-	def get(self, tag_id):
-		results = { 'Tags' : {}, 'Events' : [] }
+	def get(self, tag_id, recursive = True):
+		results = { 'Tag' : {} }
 		tag = self.db.execute('SELECT * FROM tag WHERE id = ?', [tag_id])
-		results['Tags'] = self.db.dict_row(tag.fetchone())
-		events = self.db.execute('SELECT * FROM event JOIN event_tag ON event.id = event_tag.event_id WHERE event_tag.tag_id = ?', [tag_id])
-		for event in events:
-			results['Events'].append(self.db.dict_row(event))
+		results['Tag'] = self.db.dict_row(tag.fetchone())
+
+		### we can turn off getting events
+		if recursive:
+			results['Events'] = []
+			events = self.db.execute('SELECT * FROM event JOIN event_tag ON event.id = event_tag.event_id WHERE event_tag.tag_id = ?', [tag_id])
+			for event in events:
+				results['Events'].append(self.db.dict_row(event))
 		return results
 
-	def get_all(self):
+	def get_all(self, recursive = True):
 		tags = self.db.execute('SELECT id FROM tag')
 		results = {'Tags' : []}
 		for tag in tags.fetchall():
 			print self.get(tag[0])
-			results['Tags'].append(self.get(tag[0]))
+			results['Tags'].append(self.get(tag[0], recursive))
 		return results
 
 	def save(self, data):
@@ -38,7 +42,7 @@ def get_tags():
 
 	# @todo: event data
 	tags = Tag()
-	return jsonify(tags.get_all())
+	return jsonify(tags.get_all(False))
 
 @app.route("/tags/view/<int:tag_id>")
 def get_tag(tag_id):
