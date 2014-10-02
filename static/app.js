aad41e870ac312f24c1a8dbc5cf0482f011591f5
Hammy Goonan <hammy@spiresoftware.com.au>
2014-10-02 07:34:12 +0100

    better implementation of AJAX for angular Tags controller

--- static/app.js
+++ static/app.js
@@ -1,12 +1,12 @@
 (function(){
 	var app = angular.module('timeline', []);
-	app.controller('TagsController', function($scope, $http){
-		$scope.tag_list = null;
-		$http({
-			method: 'GET',
-			url: '/tags/'
-		}).success(function(result){
-			$scope.tag_list = result.tags;
+
+	app.controller('TagsController', [ '$http', function($http){
+		var store = this;
+		store.tag_list = [];
+		$http.get('/tags/').success(function(data){
+			store.tag_list = data.tags;
 		});
-	});
+	} ]);
+
 })();
